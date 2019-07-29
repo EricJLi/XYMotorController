f4a0e460c1785ee738e8aed12944923c44237c12
EricJLi <li.eric1998@gmail.com>
2019-07-28 22:35:57 -0400

    Initial Commit

---
+++ Debug/StopWatchMode.d
@@ -0,0 +1,89 @@
+# FIXED
+
+StopWatchMode.obj: ../StopWatchMode.c
+StopWatchMode.obj: ../StopWatchMode.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430fr4133.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/ccs_base/msp430/include/in430.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics_legacy_undefs.h
+StopWatchMode.obj: ../hal_LCD.h
+StopWatchMode.obj: ../main.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/driverlib.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/stdint.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/_stdint40.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/stdint.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/cdefs.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/_types.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/machine/_types.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/machine/_stdint.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/_stdint.h
+StopWatchMode.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/stdbool.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/adc.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/crc.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/cs.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/ecomp.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/framctl.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/gpio.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/lcd_e.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/mpy32.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/pmm.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/rtc.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sac.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sfr.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sysctl.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tia.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_a.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_b.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tlv.h
+StopWatchMode.obj: C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/wdt_a.h
+
+../StopWatchMode.c: 
+../StopWatchMode.h: 
+C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430fr4133.h: 
+C:/ti/ccs910/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics_legacy_undefs.h: 
+../hal_LCD.h: 
+../main.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/driverlib.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h: 
+C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs910/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/_stdint40.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/machine/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/stdbool.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/adc.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/crc.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/cs.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/ecomp.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/framctl.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/gpio.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/lcd_e.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/mpy32.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/pmm.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/rtc.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sac.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sfr.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sysctl.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tia.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_a.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_b.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tlv.h: 
+C:/Users/ej2li/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/wdt_a.h: 
